2cdaec7ec3f73ad806a934209a6409d73db1fc86
kzky <rkzfilter@gmail.com>
2015-09-25 20:09:32 +0900

    fix errors in eclipse.

--- akka_cluster_sample/src/main/scala/edu/kzk/akka/cluster/worker_dialin/TransformationBackend.scala
+++ akka_cluster_sample/src/main/scala/edu/kzk/akka/cluster/worker_dialin/TransformationBackend.scala
@@ -1,8 +1,4 @@
-package edu.kzk.akka.cluster.worker_dialin
-
-/**
- * @author kzk
- */
+package edu.kzk.akka.cluster.worker_dialinx
 
 import language.postfixOps
 import scala.concurrent.duration._
@@ -17,43 +13,43 @@ import akka.cluster.ClusterEvent.MemberUp
 import akka.cluster.Member
 import akka.cluster.MemberStatus
 import com.typesafe.config.ConfigFactory
+import edu.kzk.akka.cluster.worker_dialin.TransformationJob
+import edu.kzk.akka.cluster.worker_dialin.TransformationResult
+import edu.kzk.akka.cluster.worker_dialin.BackendRegistration
 
 //#backend
 class TransformationBackend extends Actor {
 
-	val cluster = Cluster(context.system);
+  val cluster = Cluster(context.system)
 
-	// subscribe to cluster changes, MemberUp
-	// re-subscribe when restart
-	override def preStart(): Unit = cluster.subscribe(self, classOf[MemberUp]);
-	override def postStop(): Unit = cluster.unsubscribe(self);
+  // subscribe to cluster changes, MemberUp
+  // re-subscribe when restart
+  override def preStart(): Unit = cluster.subscribe(self, classOf[MemberUp])
+  override def postStop(): Unit = cluster.unsubscribe(self)
 
-	def receive = {
-	case TransformationJob(text) => sender() ! TransformationResult(text.toUpperCase)
-	
-  case state: CurrentClusterState =>
-  state.members.filter(_.status == MemberStatus.Up) foreach register;
-  
-  case MemberUp(m) => register(m)
-	}
+  def receive = {
+    case TransformationJob(text) => sender() ! TransformationResult(text.toUpperCase)
+    case state: CurrentClusterState =>
+      state.members.filter(_.status == MemberStatus.Up) foreach register
+    case MemberUp(m) => register(m)
+  }
 
-	def register(member: Member): Unit =
-			if (member.hasRole("frontend"))
-				context.actorSelection(RootActorPath(member.address) / "user" / "frontend") ! 
-				BackendRegistration
+  def register(member: Member): Unit =
+    if (member.hasRole("frontend"))
+      context.actorSelection(RootActorPath(member.address) / "user" / "frontend") !
+        BackendRegistration
 }
 //#backend
 
 object TransformationBackend {
-	def main(args: Array[String]): Unit = {
-			// Override the configuration of the port when specified as program argument
-			val port = if (args.isEmpty) "0" else args(0);
-					
-      val config = ConfigFactory.parseString(s"akka.remote.netty.tcp.port=$port").
-    		  withFallback(ConfigFactory.parseString("akka.cluster.roles = [backend]")).
-    		  withFallback(ConfigFactory.load());
-    		  
-      val system = ActorSystem("ClusterSystem", config);
-      system.actorOf(Props[TransformationBackend], name = "backend")
-	}
+  def main(args: Array[String]): Unit = {
+    // Override the configuration of the port when specified as program argument
+    val port = if (args.isEmpty) "0" else args(0)
+    val config = ConfigFactory.parseString(s"akka.remote.netty.tcp.port=$port").
+      withFallback(ConfigFactory.parseString("akka.cluster.roles = [backend]")).
+      withFallback(ConfigFactory.load())
+
+    val system = ActorSystem("ClusterSystem", config)
+    system.actorOf(Props[TransformationBackend], name = "backend")
+  }
 }
